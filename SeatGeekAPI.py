cf882f0770791056eeb331e3ed9a54fbee033d50
Andrew Bishara <andrew@Andrews-MacBook-Air.local>
2024-01-05 15:49:05 -0500

    Updated API url, updated variable names

--- SeatGeekAPI.py
+++ SeatGeekAPI.py
@@ -1,5 +1,7 @@
 import requests
 
-response = requests.get('https://google.com.c')
+apiKey = "https://api.seatgeek.com/2/events?client_id=Mzg0NzY2Mjl8MTcwMDcwNDA4NS45NjQ1Mzg&q={}"
+
+response = requests.get(apiKey)
 
 print(response.status_code)
